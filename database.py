03dc0f28ae8fb29242e2a29719a286bbd6965771
Alexander Villanueva <105297450+ToastyPpr@users.noreply.github.com>
2026-08-26 17:32:38 -0500

    Update database.py
    wrote something down i will change it but i didnt want to forget

--- database.py
+++ database.py
@@ -5,8 +5,8 @@ db = SQLAlchemy()
 class Lead(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
-    phone = db.Column(db.String(20), nullable=False)
-    email = db.Column(db.String(100), nullable=False)
+    phone = db.Column(db.int(10), nullable=False)
+    email = db.Column(db.String(100), nullable=False, unique = True ñ)// wrote this down before i forget it 
     details = db.Column(db.Text, nullable=True)
 
 def initialize_database(app):
